db9da0c008e8f2fb91b586ba62232046f631e5f5
krhalo <halo1235@naver.com>
2014-02-16 18:09:44 +0900

    Update logon.py

--- logon.py
+++ logon.py
@@ -1,7 +1,7 @@
 from pykakao import kakaotalk
 
 kakao = kakaotalk()
-if kakao.auth("halo1235@naver.com", "uiop1234", "HALO", "1"):
+if kakao.auth(""):
     print kakao.session_key
     print kakao.user_id
 else:
